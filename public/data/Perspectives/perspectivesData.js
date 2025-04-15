fb46515f58273309fc8f77271da45e750152764a
Satish-Nehe-P99SOFT <satish.nehe@p99soft.com>
2025-04-15 18:14:34 +0530

    Update perspectivesData.js

--- public/data/Perspectives/perspectivesData.js
+++ public/data/Perspectives/perspectivesData.js
@@ -1,9 +1,12 @@
- export const perspectiveItems = [
+
+
+
+export const perspectiveItems = [
     {
       id: 1,
       card1Title: "EU Carbon Removals and Carbon Farming Certification (CRCF)",
       industrySlug: "chemicals-and-natural-resources",
-      card1Image: "/Images/Image-1.jpg",
+      card1Image: "/images/Image-1.jpg",
       slug: "eu-carbon-removals-certification",
       card1Type: "Infographics",
       card1Description:
@@ -11,13 +14,13 @@
       card1ReadTime: 1,
       card2Type: "",
       card2Title: "Carbon Border Adjustment Mechanism (CBAM)",
-      card2image: "/Images/Image-2.jpg",
+      card2image: "/images/Image-2.jpg",
       card2Description:
         "Understanding CBAM and Its Impact As the EU imposes the first-of-its-kind Carbon Border Adjustment Mechanism (CBAM), businesses must...",
       card2ReadTime: 1,
       card3Type:"",
       card3Title: "The 2050 Transformation: Navigating the Chemical Industry's Net Zero Challenge",
-      card3Image: "/Images/Image-3.jpg",
+      card3Image: "/images/Image-3.jpg",
       cardDescription:
         "The chemical industry stands at a critical juncture. Ambitious 2050 net-zero targets loom amid geopolitical and economic tensions,...",
       card3ReadTime: 13,
@@ -26,7 +29,7 @@
       id: 2,
       card1Title: "AI in Energy Storage: Unlocking the Future of Clean Energy",
       industrySlug: "energy",
-      card1Image: "/Images/Image-4.jpg",
+      card1Image: "/images/Image-4.jpg",
       slug: "Disruptive Forces in Renewable Power Generation",
       card1Type: "",
       card1Description:
@@ -34,13 +37,13 @@
       card1ReadTime: 1,
       card2Type: "Infographics",
       card2Title: "Disruptive Forces in Renewable Power Generation",
-      card2image: "/Images/Image-5.jpg",
+      card2image: "/images/Image-5.jpg",
       card2Description:
         "The renewable energy sector is accelerating into a new era—driven by disruptive innovation, AI-powered intelligence, and  decarbonizatio",
       card2ReadTime: 2,
       card3Type: "",
       card3Title: "Electric Motors – State-of-art and Way Ahead",
-      card3Image: "/Images/Image-6.jpg",
+      card3Image: "/images/Image-6.jpg",
       cardDescription:
         "Electric motors account for nearly half of global electricity use. Even a modest 1% increase in efficiency can have a significant impact on energy...",
       card3ReadTime: 1,
@@ -49,7 +52,7 @@
       id: 3,
       card1Title: "FoodTech Trends Series – Valorizing the Unusable",
       industrySlug: "food-and-nutrition",
-      card1Image: "/Images/Image-7.jpg",
+      card1Image: "/images/Image-7.jpg",
       slug: "FoodTech Trends Series – Valorizing the Unusable",
       card1Type: "",
       card1Description:
@@ -57,13 +60,13 @@
       card1ReadTime: 1,
       card2Type: "",
       card2Title: "Macro Forces shaping the Food and Nutrition Industry",
-      card2image: "/Images/Image-8.jpg",
+      card2image: "/images/Image-8.jpg",
       card2Description:
         "What Are the Expected Food Trends Shaping the Future? The food and nutrition industry is entering a transformative phase. As consume...",
       card2ReadTime: 2,
       card3Type: "Article",
       card3Title: "Zero Cow Factory",
-      card3Image: "/Images/Image-9.jpg",
+      card3Image: "/images/Image-9.jpg",
       cardDescription:
         "Startup Spotlight: Zero Cow Factory Location: India   Total Funding: $4 Million  Last Funding Round: Seed | ~ $4M | April 2023 Key Investors: Brinc, ...",
       card3ReadTime: 2,
@@ -72,7 +75,7 @@
       id: 4,
       card1Title: "The Quiet Boom: Evolving Perceptions of Non-Invasive & Preventive Male Aesthetics",
       industrySlug: "home-and-personal-care",
-      card1Image: "/Images/Image-10.jpg",
+      card1Image: "/images/Image-10.jpg",
       slug: "FoodTech Trends Series – Valorizing the Unusable",
       card1Type: "",
       card1Description:
@@ -80,13 +83,13 @@
       card1ReadTime: 1,
       card2Type: "Case Studies",
       card2Title: "Scent-Sational Strategies from FutureBridge Drive Sustainable Innovation in Fragrances",
-      card2image: "/Images/Image-11.jpg",
+      card2image: "/images/Image-11.jpg",
       card2Description:
         "Quick Overview Our client, a leading deodorant and fragrance provider in Europe and North America, sought to identify sustainable alternatives, ...",
       card2ReadTime: 4,
       card3Type: "Case Studies",
       card3Title: "Driving Circularity and Innovation in Personal Care through Crucial Insights and",
-      card3Image: "/Images/Image-12.jpg",
+      card3Image: "/images/Image-12.jpg",
       cardDescription:
         "Quick Overview A progressive beauty brand sought to elevate its innovation strategy by integrating cutting-edge trends and sustainability practice, ...",
       card3ReadTime: 4,
@@ -95,7 +98,7 @@
       id: 5,
       card1Title: "Skin Microbiome 2.0 is Ditching the Clean Beauty Myth",
       industrySlug: "life-sciences",
-      card1Image: "/Images/Image-13.jpg",
+      card1Image: "/images/Image-13.jpg",
       slug: "Skin Microbiome 2.0 is Ditching the Clean Beauty Myth",
       card1Type: "Video and Audio",
       card1Description:
@@ -103,13 +106,13 @@
       card1ReadTime: 2,
       card2Type: "Video and Audio",
       card2Title: "The Quiet Boom: Evolving Perceptions of Non-Invasive & Preventive Male Aesthetics",
-      card2image: "/Images/Image-14.jpg",
+      card2image: "/images/Image-14.jpg",
       card2Description:
         "The male aesthetics market is experiencing significant growth, reflecting a shift in men’s grooming and self-care practices. Valued  ...",
       card2ReadTime: 1,
       card3Type: "Video and Audio",
       card3Title: "Smart MedTech: Unveiling AI-Powered Innovations in Medical Devices",
-      card3Image: "/Images/Image-15.jpg",
+      card3Image: "/images/Image-15.jpg",
       cardDescription:
         "AI is accelerating diagnostics, enhancing treatment precision, and streamlining regulatory approvals in medical devices. Companies t, ...",
       card3ReadTime: 2,
@@ -118,7 +121,7 @@
         id: 6,
         card1Title: "Growing Role of Robots in Construction & Industrial Services",
         industrySlug: "industrial-equipment",
-        card1Image: "/Images/Image-16.jpg",
+        card1Image: "/images/Image-16.jpg",
         slug: "Growing Role of Robots in Construction & Industrial Services",
         card1Type: "",
         card1Description:
@@ -126,13 +129,13 @@
         card1ReadTime: 1,
         card2Type: "Video and Audio",
         card2Title: "AI and Automation Transforming R&D",
-        card2image: "/Images/Image-17.jpg",
+        card2image: "/images/Image-17.jpg",
         card2Description:
           "This executive session breaks down how AI is being operationalized across R&D pipelines. Hear from leaders executing high-impact   ...",
         card2ReadTime: 1,
         card3Type: "",
         card3Title: "AI and Automation Transforming R&D",
-        card3Image: "/Images/Image-18.jpg",
+        card3Image: "/images/Image-18.jpg",
         cardDescription:
           "AI and automation are transforming how R&D functions—cutting inefficiencies, speeding up development, and unlocking, ...",
         card3ReadTime: 1,
@@ -141,7 +144,7 @@
       id: 7,
       card1Title: "eVTOLs: Taking off to a Sustainable Future- A Detailed Exploration of Opportunities and Challenges",
       industrySlug: "mobility",
-      card1Image: "/Images/Image-19.jpg",
+      card1Image: "/images/Image-19.jpg",
       slug: "eVTOLs: Taking off to a Sustainable Future- A Detailed Exploration of Opportunities and Challenges",
       card1Type: "",
       card1Description:
@@ -149,13 +152,13 @@
       card1ReadTime: 3,
       card2Type: "",
       card2Title: "The Future of Mobility: Key Trends Shaping 2024 and Beyond in Electric, Autonomo",
-      card2image: "/Images/Image-20.jpg",
+      card2image: "/images/Image-20.jpg",
       card2Description:
         "The mobility landscape is undergoing a seismic shift. As we approach 2025, the demand for more sustainable, efficient, and user-centric...",
       card2ReadTime: 5,
       card3Type: "Article",
       card3Title: "The Future of AI – Consolidation, Competition, and First-Mover Advantages",
-      card3Image: "/Images/Image-21.jpg",
+      card3Image: "/images/Image-21.jpg",
       cardDescription:
         "Executive Summary Tech majors are in a race to acquire more data and computing power to train and commercialize complex AI models, ...",
       card3ReadTime: 9,
